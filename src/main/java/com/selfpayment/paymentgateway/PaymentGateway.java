727ccf9e35bd570f9f3190cbbd6db6bfac59e7bd
Piseth KHON <piseth.khon@presta.tessi.fr>
2023-07-11 16:29:26 +0700

    Initial project

---
+++ src/main/java/com/selfpayment/paymentgateway/PaymentGateway.java
@@ -0,0 +1,17 @@
+package com.selfpayment.paymentgateway;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API Gateway", version = "1.0", description = "Documentation API Gateway v1.0"))
+public class PaymentGateway {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PaymentGateway.class, args);
+  }
+}
